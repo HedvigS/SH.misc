6d7b0a5e97d2a485b6265a574afa5bdb43492f93
HedvigS <hedvig.skirgard@gmail.com>
2024-06-07 10:22:28 +0100

    Update credit_packages.R

--- R/credit_packages.R
+++ R/credit_packages.R
@@ -1,23 +1,34 @@
 #' Lists used packages in a set of R-script and generates citation keys.
 #'
 #' @param input_dir character vector. Name of directory of R-scripts. Will be serched downwards recursively, i.e. also go through sub-directories.
-#' @param output_dir character vector. Name of directory to print bibTeX file and citation-keys. Necessary if print_bibtex == TRUE and/or print_tex_citation_string == TRUE.
-#' @param print_bibtex logical. If TRUE, a bibTeX file is written with entries for the packages found to be used. File will be written to output_dir as "used_pkgs.bib"
+#' @param pkgs_vec character vector. Names of R-packages, either instead of input_dir or in addition. Sometimes some packages aren't missed when searching through the scripts, if you add their names here they'll be included in the output.
+#' @param output_dir character vector. Name of directory to print bibTeX file and citation-keys. Necessary if print_bibTeX == TRUE and/or print_tex_citation_string == TRUE.
+#' @param print_bibTeX logical. If TRUE, a bibTeX file is written with entries for the packages found to be used. File will be written to output_dir as "used_pkgs.bib"
+#' @param print_LaTeX_table logical. If TRUE, a LaTeX table will be rendered with each pagkage as a row and a column for version loaded.
+#' @param print_df logical. If TRUE, a table of packages will be printed to a tsv-file in the output_dir, with a column for version loaded.
 #' @param print_tex_citation_string logical. If TRUE, a txt-file will be written with a string of all the TeX citation keys in a string. This txt-file can be included in a TeX document with "\\input{}". It will be in output_dir with the name "citation_keys.txt".
 #' @param compare_loaded_with_used logical. If TRUE, the set of packages that the scripts used are compared to those loaded currently in the environment and reported in terminal.
 #' @param report_most_used_pkgs logical. If TRUE, the function will report which are the 5 packages you use the most functions from to the terminal.
 #' @param report_script_with_most_funs logical. If TRUE, the function will report which script has the most function calls to the terminal.
 #' @param verbose logical. If TRUE, the function will be more talkative.
 #' @return Data-frame of all used functions. Depending on the arguments, the function also returns output to the terminal and/or files written to the output directory.
+#' @import dplyr
+#' @import magrittr
+#' @import tidyr
+#' @import naniar
+#' @import xtable
 #' @note In cases where it is not clear which specific package a function is from several packages are returned for that function. This is for example the case with filter().
 #' @export
 #'
 credit_packages <- function(input_dir = NULL,
+                            pkgs_vec = NULL,
                             output_dir = NULL,
-                            print_bibtex = TRUE,
+                            print_bibTeX = TRUE,
                             print_tex_citation_string = TRUE,
                             compare_loaded_with_used = TRUE,
                             report_most_used_pkgs = TRUE,
+                            print_LaTeX_table = TRUE,
+                            print_df = TRUE,
                             report_script_with_most_funs = TRUE,
                             verbose = TRUE
                             ){
@@ -25,9 +36,20 @@ credit_packages <- function(input_dir = NULL,
 #input_dir = "../../Oceanic_computational_ASR/code/"
 #output_dir <- "."
 
+if(all(is.null(input_dir), is.null(pkgs_vec))){
+    stop("Neither input_dir nor pkgs_vec has been supplied.")
+}
+
+    if(is.null(output_dir)){
+        stop("output_dir not supplied.")
+    }
+
+
+
+    if(!is.null(input_dir)){
+
 r_fns <- list.files(path = input_dir, pattern = "*.[R|r]$", full.names = T, recursive = T)
 
-library(dplyr)
 # df to bind to
 df <- data.frame("packages" = as.character(),
                  "functions" = as.character(),
@@ -40,7 +62,7 @@ for(fn in r_fns){
     }
 
     #fn <- r_fns[3]
-x <- list.functions.in.file_SH(filename = fn) %>%
+x <- .list.functions.in.file_SH(filename = fn) %>%
     as.data.frame() %>%
     tibble::rownames_to_column("packages") %>%
     dplyr::rename("functions" = 2) %>%
@@ -64,6 +86,10 @@ used_packages <- df %>%
 used_packages <- naniar::replace_with_na(data = used_packages, replace= list(packages = ""))
 used_packages$packages <- trimws(used_packages$packages)
 
+used_packages <- used_packages %>%
+    dplyr::filter(packages != ".GlobalEnv") %>%
+    dplyr::filter(!is.na(packages))
+
 #df with loaded packages
 if(compare_loaded_with_used == TRUE){
 
@@ -81,13 +107,13 @@ cat("There are ", nrow(unused_but_loaded), "packages that it seems like you're n
 
 }
 
+
 if(report_most_used_pkgs  == TRUE){
+
 most_used <- used_packages %>%
-    dplyr::group_by(packages) %>%
-    dplyr::summarise(n = n()) %>%
-    dplyr::arrange(desc(n)) %>%
-    dplyr::filter(packages != ".GlobalEnv") %>%
-    dplyr::filter(!is.na(packages))
+        dplyr::group_by(packages) %>%
+        dplyr::summarise(n = n()) %>%
+        dplyr::arrange(desc(n))
 
 cat("The top 5 packages you used the most:\n ")
 print(as.matrix(
@@ -105,26 +131,56 @@ print(as.matrix(
 script_with_most_functions [1:5,])
 )
 }
+    }
 
-if(print_bibtex == TRUE){
+    if(is.null(input_dir) & !is.null(pkgs_vec)){
+        pkgs_to_cite <- pkgs_vec %>% unique()
+            }
+    if(!is.null(input_dir) & !is.null(pkgs_vec)){
+        pkgs_to_cite <- unique(c(used_packages$packages, pkgs_vec))
+    }
+
+    if(!is.null(input_dir) & is.null(pkgs_vec)){
+        pkgs_to_cite <- unique(c(used_packages$packages))
+    }
+
+
+
+
+if(print_bibTeX == TRUE){
 
     output_fn <- paste0(output_dir, "/used_pkgs.bib")
 
-    knitr::write_bib(as.character(unique(most_used$packages)), file = output_fn)
+    knitr::write_bib(as.character(pkgs_to_cite), file = output_fn)
 
 readLines(output_fn) %>%
   stringr::str_replace_all("\\&", "\\\\&") %>% #sorting out issues with ampersand
   stringr::str_replace_all("\\\\\\\\&", "\\\\&") %>%
     writeLines(output_fn)
 
+##adding in citation for R itself
+
+#https://stackoverflow.com/questions/46179997/r-missing-bib-key-in-citation-output
+R_bib = citation()
+R_bib$key = paste0("R_", R.version$major, ".", R.version$minor)
+R_bib$note <- R.version.string
+R_bib_string <- utils::toBibtex(R_bib)
+
+bib <- readLines(output_fn)
+bib <- c(bib, R_bib_string)
+bib %>% write_lines(output_fn)
+
 if(verbose == TRUE){
 
-cat(paste0("Wrote citations for packages you've used to ", output_fn, ".\n There were ", length(!is.na(used_packages$packages %>% unique()))
-, " entries.\n" ))
+cat(paste0(
+        "There were ", length(pkgs_to_cite)
+, " packages to cite.\n" ))
+
+cat(paste0("Wrote ", output_fn, ".\n"))
     }
 }
 
-#optional part, this generates a text string with the bibtex citation KEYS from earlier that you can then paste into LaTeX in order to cite all
+#optional part, this generates a text string with the bibTeX citation KEYS from earlier that you can then paste into LaTeX in order to cite all
 
 if(print_tex_citation_string == TRUE){
 bibdf <- suppressWarnings(bib2df::bib2df(output_fn))
@@ -137,16 +193,58 @@ vec <- paste0("\\citet{", bibdf$BIBTEXKEY, "}")
 
 fPaste(vec)   %>%
   writeLines(con = paste0(output_dir, "/citation_keys.txt"))
+
+if(verbose == TRUE){
+    cat(paste0("Wrote ", output_dir, "/citation_keys.txt") )
 }
-used_packages
+
+}
+
+    pkgs_to_cite_df <- installed.packages()[pkgs_to_cite, "Version"] %>%
+        as.data.frame() %>%
+        rownames_to_column("Package") %>%
+        rename("Version" = "." )
+
+    if(print_df == TRUE ){
+    pkgs_to_cite_df %>%
+        write_tsv(paste0(output_dir,"/pkgs_versions_table.tsv"))
+
+        if(verbose == TRUE){
+            cat(paste0("Wrote ", output_dir,"/pkgs_versions_table.tsv \n"))
+        }
+
+
+
+    }
+
+    if(print_LaTeX_table == TRUE ){
+
+        fn_out = paste0(output_dir, "/used_packages_table.tex")
+        cap <- "Table of R-packages used."
+        lbl <- "r_package_table"
+        align <- c("r","p{2.5cm}","p{2.5cm}")
+
+        pkgs_to_cite_df %>%
+            xtable::xtable(caption = cap, label = lbl,
+                   align = align) %>%
+            xtable::print.xtable(file = fn_out,
+                                 sanitize.colnames.function = function(x){x},
+                                 sanitize.text.function = function(x){x},
+                                 include.rownames = FALSE, math.style.negative = F,tabular.environment = "longtable",
+                                 booktabs = TRUE, floating = F)
+
+        if(verbose == TRUE){
+            cat(paste0("Wrote ", fn_out, "\n" ))
+        }
+
+
+    }
+
+
 }
 
 
-## Function taken from NCmisc, but removing unique() so that you get one line per function call.
-
-library(NCmisc)
-
-list.functions.in.file_SH <- function (filename, alphabetic = TRUE)
+.list.functions.in.file_SH <- function (filename, alphabetic = TRUE)
 {
     if (!file.exists(filename)) {
         stop("couldn't find file ", filename)
@@ -166,3 +264,4 @@ list.functions.in.file_SH <- function (filename, alphabetic = TRUE)
     outlist <- tapply(funs, factor(src), c)
     return(outlist)
 }
+
