20f1ea111997a1f413b191ca017aa9644f3fb6fe
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-06-24 14:30:25 +0200

    Update stack_tsvs.R

--- R/stack_tsvs.R
+++ R/stack_tsvs.R
@@ -4,6 +4,7 @@
 #' @param pattern character vector. Optional, if specified the function will only deal with the tsvs:s that match the pattern. Do not specify "tsv", that is already specified.
 #' @param recursive logical. If TRUE, tsvs in dirs inside the dir specified will also be matched.
 #' @param range numeric vector specifying a range to limit the vector of file-names to read in by. Default is NULL, i.e. all files are read in. Sometimes if can be desirable to subset the files to test, if so the range can be set to c(1:10) for the first 10 files.
+#' @param verbose logical. If TRUE, function will report on which file it is at etc.
 #' @return data-frame with content of all tsvs stacked and a column with filename. 
 #' @note All content will be turned into character to facilitate joining and then the col-types will be converted back to appropriate class for the whole data-frame.
 #' @author Hedvig Skirgård.
@@ -18,30 +19,42 @@
 stack_tsvs <- function(dir = NULL, 
                        pattern = NULL, 
                        recursive = FALSE, 
-                       range = NULL){
+                       range = NULL, 
+                       verbose= TRUE){
 
 
   fns <- list.files(path = dir, pattern = paste0(pattern, ".*tsv$"), full.names = T, recursive = recursive)
 
   if(!is.null(range)){
-    fns <-   fns[range]    
+    fns <-   fns[range]
     }
+  
+  if(verbose == TRUE){
+    cat("I'm reading in ", length(fns), " files.\n"
+      , sep = "")
+    
+      }
 
-  All_raw <- fns %>% 
-    purrr::map_df(
-      function(x) data.table::fread(x ,
-                                    encoding = 'UTF-8', header = TRUE, 
-                                    fill = TRUE, blank.lines.skip = TRUE,
-                                    sep = "\t", na.strings = "",
-      )   %>% 
-        dplyr::mutate(across(everything(), as.character)) %>% 
-        dplyr::mutate(filename =x)
-               
-      
-    ) 
+read_for_map_df <- function(x){ 
+  if(verbose == TRUE){
+  cat("I'm at file ", x, ".\n")
+  }
+  df <- data.table::fread(x ,
+                                encoding = 'UTF-8', header = TRUE, 
+                                fill = TRUE, blank.lines.skip = TRUE,
+                                sep = "\t", na.strings = "",
+  )   %>% 
+      dplyr::mutate(across(everything(), as.character)) %>% 
+      dplyr::mutate(filename =x)
+    df
+  }  
+  
+  All_raw <-    purrr::map_df(.x = fns, .f = read_for_map_df)
   
 All_raw <- suppressMessages(readr::type_convert(All_raw, trim_ws = TRUE, guess_integer = FALSE))
   
 
 All_raw  
 }
+
+library(tidyverse)
