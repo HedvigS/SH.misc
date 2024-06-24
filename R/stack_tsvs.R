dbe1bde36ff29f2d43dde39e1c236f984fa04dba
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2024-06-24 11:38:25 +0200

    add range option

--- R/stack_tsvs.R
+++ R/stack_tsvs.R
@@ -3,6 +3,7 @@
 #' @param dir character vector. file.path to directory the function is to search in.
 #' @param pattern character vector. Optional, if specified the function will only deal with the tsvs:s that match the pattern. Do not specify "tsv", that is already specified.
 #' @param recursive logical. If TRUE, tsvs in dirs inside the dir specified will also be matched.
+#' @param range numeric vector specifying a range to limit the vector of file-names to read in by. Default is NULL, i.e. all files are read in. Sometimes if can be desirable to subset the files to test, if so the range can be set to c(1:10) for the first 10 files.
 #' @return data-frame with content of all tsvs stacked and a column with filename. 
 #' @note All content will be turned into character to facilitate joining and then the col-types will be converted back to appropriate class for the whole data-frame.
 #' @author Hedvig Skirgård.
@@ -12,16 +13,19 @@
 #' @import readr
 #' 
 
-dir = "../../Glottobank/Grambank/original_sheets/"
-pattern = "HS"
+#dir = "../../Glottobank/Grambank/original_sheets/"
 
 stack_tsvs <- function(dir = NULL, 
                        pattern = NULL, 
-                       recursive = recursive){
+                       recursive = FALSE, 
+                       range = NULL){
 
 
   fns <- list.files(path = dir, pattern = paste0(pattern, ".*tsv$"), full.names = T, recursive = recursive)
-  
+
+  if(!is.null(range)){
+    fns <-   fns[range]    
+    }
 
   All_raw <- fns %>% 
     purrr::map_df(
