dcbac91891ec93a06666e0cb323700f8f923fbbd
Hedvig Skirgård <hedvig.skirgard@gmail.com>
2026-03-31 16:53:19 +0200

    Replace exclude_base with pkgs_to_ignore parameter

--- R/credit_packages.R
+++ R/credit_packages.R
@@ -10,7 +10,7 @@
 #' @param compare_loaded_with_used logical. If TRUE, the set of packages that the scripts used are compared to those loaded currently in the environment and reported in terminal.
 #' @param report_most_used_pkgs logical. If TRUE, the function will report which are the 5 packages you use the most functions from to the terminal.
 #' @param report_script_with_most_funs logical. If TRUE, the function will report which script has the most function calls to the terminal.
-#' @param exclude_base logical. If TRUE, base packages ("compiler" ,  "graphics" ,  "tools" ,     "rstudioapi" ,"utils"  ,    "grDevices",  "stats" ,  "datasets" ,  "methods"  ,  "base") are excluded.
+#' @param pkgs_to_ignore character vector. If not NULL, the packages listed will be ignored from the process.
 #' @param verbose logical. If TRUE, the function will be more talkative.
 #' @return Data-frame of all used functions. Depending on the arguments, the function also returns output to the terminal and/or files written to the output directory.
 #' @author Hedvig Skirgård
@@ -27,275 +27,279 @@ credit_packages <- function(fns = NULL,
                             print_LaTeX_table = TRUE,
                             print_tsv = TRUE,
                             report_script_with_most_funs = TRUE,
-                            exclude_base = TRUE,
+                            pkgs_to_ignore = c("compiler" ,  "graphics" ,  "tools" ,     "rstudioapi" ,"utils"  ,    "grDevices",  "stats" ,  "datasets" ,  "methods"  ,  "base"),
                             verbose = TRUE
-                            ){
-
-if(all(is.null(fns), is.null(pkgs_vec_manual))){
+){
+  
+  if(all(is.null(fns), is.null(pkgs_vec_manual))){
     stop("Neither fns nor pkgs_vec_manual has been supplied.")
-}
-
-    if(is.null(output_dir)){
-        stop("output_dir not supplied.")
-    }else{
-          if(!dir.exists(output_dir)){dir.create(output_dir)}
+  }
+  
+  if(is.null(output_dir)){
+    stop("output_dir not supplied.")
+  }else{
+    if(!dir.exists(output_dir)){dir.create(output_dir)}
+  }
+  
+  if(!is.null(fns)){
+    
+    # df to bind to
+    df <- data.frame("packages" = as.character(),
+                     "functions" = as.character(),
+                     "script" = as.character())
+    
+    for(fn in fns){
+      
+      if(verbose == TRUE){
+        cat(paste0("I'm on ", fn, "\n"))
       }
-
-    if(!is.null(fns)){
-
-# df to bind to
-df <- data.frame("packages" = as.character(),
-                 "functions" = as.character(),
-                 "script" = as.character())
-
-for(fn in fns){
-
-        if(verbose == TRUE){
-  cat(paste0("I'm on ", fn, "\n"))
+      
+      #fn <- fns[3]
+      x <- .list.functions.in.file_SH(filename = fn) %>%
+        dplyr::mutate(packages = stringr::str_replace_all(packages, "package:", "")) %>%
+        dplyr::mutate(packages = stringr::str_replace_all(packages, "c\\(", "")) %>%
+        dplyr::mutate(packages = stringr::str_replace_all(packages, "\\)", "")) %>%
+        dplyr::mutate(packages = stringr::str_replace_all(packages, "\\\"", "")) %>%
+        dplyr::mutate(packages = stringr::str_replace_all(packages, "character\\(0", "")) %>%
+        dplyr::mutate(packages = stringr::str_split(packages, ";")) %>%
+        tidyr::unnest(cols = "packages") %>%
+        dplyr::mutate(script = fn)
+      
+      df <- dplyr::full_join(x, df, by = c("packages", "functions", "script"))
     }
-
-    #fn <- fns[3]
-x <- .list.functions.in.file_SH(filename = fn) %>%
-    dplyr::mutate(packages = stringr::str_replace_all(packages, "package:", "")) %>%
-    dplyr::mutate(packages = stringr::str_replace_all(packages, "c\\(", "")) %>%
-    dplyr::mutate(packages = stringr::str_replace_all(packages, "\\)", "")) %>%
-    dplyr::mutate(packages = stringr::str_replace_all(packages, "\\\"", "")) %>%
-    dplyr::mutate(packages = stringr::str_replace_all(packages, "character\\(0", "")) %>%
-    dplyr::mutate(packages = stringr::str_split(packages, ";")) %>%
-    tidyr::unnest(cols = "packages") %>%
-    dplyr::mutate(script = fn)
-
-df <- dplyr::full_join(x, df, by = c("packages", "functions", "script"))
-}
-
-if(exclude_base == TRUE){
-base_R_packages <- c("compiler" ,  "graphics" ,  "tools" ,     "rstudioapi" ,"utils"  ,    "grDevices",  "stats" ,  "datasets" ,  "methods"  ,  "base")
-
-df <- df %>% 
-  dplyr::filter(!`packages` %in% base_R_packages)
-}
-
-used_packages <- df %>%
-  dplyr::mutate(used = "TRUE") %>%
-  filter(packages != "") %>%
-  filter(!is.na(packages) )
-
-
-# removing instances where a package wasn't found
-used_packages$packages <- trimws(used_packages$packages)
-
-used_packages <- used_packages %>%
-    dplyr::filter(packages != ".GlobalEnv") %>%
-    dplyr::filter(!is.na(packages)|
-                    packages != "")
-
-#df with loaded packages
-if(compare_loaded_with_used == TRUE){
-
-loaded_packages <- data.frame(packages = (.packages())) %>%
-  dplyr::mutate(loaded = "TRUE")
-
-joined_df <- dplyr::full_join(used_packages, loaded_packages, by = "packages")
-
-unused_but_loaded <- joined_df %>%
-    dplyr::filter(is.na(used)) %>%
-    dplyr::filter(!is.na(loaded)) %>%
-    dplyr::distinct(packages)[,1] %>% as.vector()
-
-warning("There are packages that it seems like you're not using, but that are still loaded in the environment. Please note that these could be crucial depdencies of other packages, but they don't contain functions that you are calling directly. Do not remove from loading without careful inspection. They loaded but seemingly not used packages are: \n ", unused_but_loaded, "\n If you don't want to check this, set 'compare_loaded_with_used' to FALSE.\n\n" )
-
-}
-
-if(report_most_used_pkgs  == TRUE){
-
-most_used <- used_packages %>%
+    
+    if(!is.null(pkgs_to_ignore)){
+      df <- df %>% 
+        dplyr::filter(!`packages` %in% pkgs_to_ignore)
+    }
+    
+    used_packages <- df %>%
+      dplyr::mutate(used = "TRUE") %>%
+      filter(packages != "") %>%
+      filter(!is.na(packages) )
+    
+    
+    # removing instances where a package wasn't found
+    used_packages$packages <- trimws(used_packages$packages)
+    
+    used_packages <- used_packages %>%
+      dplyr::filter(packages != ".GlobalEnv") %>%
+      dplyr::filter(!is.na(packages)|
+                      packages != "")
+    
+    #df with loaded packages
+    if(compare_loaded_with_used == TRUE){
+      
+      loaded_packages <- data.frame(packages = (.packages())) %>%
+        dplyr::mutate(loaded = "TRUE")
+      
+      if(!is.null(pkgs_to_ignore)){
+        loaded_packages <- loaded_packages %>% 
+          dplyr::filter(!`packages` %in% pkgs_to_ignore)
+      }
+      
+      joined_df <- dplyr::full_join(used_packages, loaded_packages, by = "packages")
+      
+      unused_but_loaded <- joined_df %>%
+        dplyr::filter(is.na(used)) %>%
+        dplyr::filter(!is.na(loaded)) %>%
+        dplyr::pull(packages) %>% unique() %>% na.omit()
+      
+      warning("There are packages that it seems like you're not using, but that are still loaded in the environment. Please note that these could be crucial depdencies of other packages, but they don't contain functions that you are calling directly. Do not remove from loading without careful inspection. They loaded but seemingly not used packages are: ", paste0( unused_but_loaded, collapse = ", " ), "\n If you don't want to check this, set 'compare_loaded_with_used' to FALSE.\n\n" )
+      
+    }
+    
+    if(report_most_used_pkgs  == TRUE){
+      
+      most_used <- used_packages %>%
         dplyr::group_by(packages) %>%
         dplyr::summarise(n = n()) %>%
         dplyr::arrange(desc(n))
-
-cat("The top 5 packages you used the most:\n ")
-print(as.matrix(
-most_used[1:5,]))
-}
-
-if(report_script_with_most_funs == TRUE){
-script_with_most_functions <-  used_packages %>%
-    dplyr::group_by(script) %>%
-    dplyr::summarise(n = n()) %>%
-    dplyr::arrange(desc(n))
-
-cat("The top 5 scripts which use the most functions:\n ")
-print(as.matrix(
-script_with_most_functions [1:5,])
-)
-}
-
-
-
-if("" %in% df$packages & verbose == TRUE){
-
-  not_matched <-   df %>%
-    filter(packages == ""|is.na(packages)) %>%
-    distinct("function")[,1] %>% as.vector()
-
-  warning("There were some functions that couldn't be matched to packages. This could be because the package isn't loaded in this session. Run requirements.R or similar and run the function again. Another possible cause is that the functions don't belong to packages at all but were defined elsewhere. The functions that cannot be matched to packages are:.\n ", not_matched, "\n\n" )
-
-}
+      
+      cat("The top 5 packages you used the most:\n ")
+      print(as.matrix(
+        most_used[1:5,]))
+    }
+    
+    if(report_script_with_most_funs == TRUE){
+      script_with_most_functions <-  used_packages %>%
+        dplyr::group_by(script) %>%
+        dplyr::summarise(n = n()) %>%
+        dplyr::arrange(desc(n))
+      
+      cat("The top 5 scripts which use the most functions:\n ")
+      print(as.matrix(
+        script_with_most_functions [1:5,])
+      )
+    }
+    
+    
+    
+    if("" %in% df$packages & verbose == TRUE){
+      
+      not_matched <-   df %>%
+        filter(packages == ""|is.na(packages)) %>%
+        dplyr::pull(`functions`) %>% unique() %>% na.omit()
+      
+      warning("There were some functions that couldn't be matched to packages. This could be because the package isn't loaded in this session. Run requirements.R or similar and run the function again. Another possible cause is that the functions don't belong to packages at all but were defined elsewhere. The functions that cannot be matched to packages are: ", paste0(not_matched, collapse = ", "), "\n" )
+      
+      
+    }
   }
-
-      if(is.null(fns) & !is.null(pkgs_vec_manual)){
-        pkgs_to_cite <- pkgs_vec_manual %>% unique()
-            }
-    if(!is.null(fns) & !is.null(pkgs_vec_manual)){
-        pkgs_to_cite <- unique(c(used_packages$packages, pkgs_vec_manual))
-    }
-
-    if(!is.null(fns) & is.null(pkgs_vec_manual)){
-        pkgs_to_cite <- unique(c(used_packages$packages))
-    }
-
-
-if(print_bibTeX == TRUE){
-
+  
+  if(is.null(fns) & !is.null(pkgs_vec_manual)){
+    pkgs_to_cite <- pkgs_vec_manual %>% unique()
+  }
+  if(!is.null(fns) & !is.null(pkgs_vec_manual)){
+    pkgs_to_cite <- unique(c(used_packages$packages, pkgs_vec_manual))
+  }
+  
+  if(!is.null(fns) & is.null(pkgs_vec_manual)){
+    pkgs_to_cite <- unique(c(used_packages$packages))
+  }
+  
+  
+  if(print_bibTeX == TRUE){
+    
     output_fn <- paste0(output_dir, "/used_pkgs.bib")
-
+    
     knitr::write_bib(as.character(pkgs_to_cite), file = output_fn, tweak = T)
-
+    
     # --- fix trailing commas in author fields and unescaped ampersands ---
     bib <- readLines(output_fn)
-
+    
     bib <- gsub(
-        "(author\\s*=\\s*\\{[^}]*),\\s*\\}",
-        "\\1}",
-        bib
+      "(author\\s*=\\s*\\{[^}]*),\\s*\\}",
+      "\\1}",
+      bib
     )
-
-bib <-   bib %>%
-        stringr::str_replace_all("\\&", "\\\\&") %>% #sorting out issues with ampersand
+    
+    bib <-   bib %>%
+      stringr::str_replace_all("\\&", "\\\\&") %>% #sorting out issues with ampersand
       stringr::str_replace_all("\\\\\\\\&", "\\\\&")
-
-
+    
+    
     writeLines(bib, output_fn)
-
-##adding in citation for R itself
-
-#https://stackoverflow.com/questions/46179997/r-missing-bib-key-in-citation-output
-R_bib = utils::citation(package = "base")
-R_bib$key = paste0("R_", R.version$major, ".", R.version$minor)
-R_bib$note <- R.version.string
-R_bib_string <- utils::toBibtex(R_bib)
-
-bib <- readLines(output_fn)
-bib <- c(bib, R_bib_string)
-bib %>% writeLines(output_fn)
-
-if(verbose == TRUE){
-
-cat(paste0(
+    
+    ##adding in citation for R itself
+    
+    #https://stackoverflow.com/questions/46179997/r-missing-bib-key-in-citation-output
+    R_bib = utils::citation(package = "base")
+    R_bib$key = paste0("R_", R.version$major, ".", R.version$minor)
+    R_bib$note <- R.version.string
+    R_bib_string <- utils::toBibtex(R_bib)
+    
+    bib <- readLines(output_fn)
+    bib <- c(bib, R_bib_string)
+    bib %>% writeLines(output_fn)
+    
+    if(verbose == TRUE){
+      
+      cat(paste0(
         "There were ", length(pkgs_to_cite)
-, " packages to cite. They are:\n" ))
-        print(pkgs_to_cite)
-
-cat(paste0("Wrote ", output_fn, ".\n"))
+        , " packages to cite. They are:\n" ))
+      print(pkgs_to_cite)
+      
+      cat(paste0("Wrote ", output_fn, ".\n"))
     }
-}
-
-#optional part, this generates a text string with the bibTeX citation KEYS from earlier that you can then paste into LaTeX in order to cite all
-
-if(print_tex_citation_string == TRUE){
-bibdf <- suppressWarnings(bib2df::bib2df(output_fn))
-
-#solution to getting an "and" on the last line from SO
-# https://stackoverflow.com/questions/42456452/changing-vector-to-string-special-separator-for-last-element
-fPaste <- function(vec) sub(",\\s+([^,]+)$", " and \\1", toString(vec))
-
-vec <- paste0("\\citet{", bibdf$BIBTEXKEY, "}")
-
-fPaste(vec)   %>%
-  writeLines(con = paste0(output_dir, "/citation_keys.txt"))
-
-if(verbose == TRUE){
-    cat(paste0("Wrote ", output_dir, "/citation_keys.txt") )
-}
-
-}
-
-#    install.packages("pkgsearch")
-#    library(pkgsearch)
-#    pkg_info <- pkgsearch::cran_package("dplyr", version = "1.1.4")
-#    pkg_info$`Date/Publication`
-
-
-    pkgs_to_cite_df <- utils::installed.packages()[pkgs_to_cite, "Version"] %>%
-        as.data.frame() %>%
-        tibble::rownames_to_column("Package") %>%
-        dplyr::rename("Version" = "." )
-
-    if(print_tsv == TRUE ){
+  }
+  
+  #optional part, this generates a text string with the bibTeX citation KEYS from earlier that you can then paste into LaTeX in order to cite all
+  
+  if(print_tex_citation_string == TRUE){
+    bibdf <- suppressWarnings(bib2df::bib2df(output_fn))
+    
+    #solution to getting an "and" on the last line from SO
+    # https://stackoverflow.com/questions/42456452/changing-vector-to-string-special-separator-for-last-element
+    fPaste <- function(vec) sub(",\\s+([^,]+)$", " and \\1", toString(vec))
+    
+    vec <- paste0("\\citet{", bibdf$BIBTEXKEY, "}")
+    
+    fPaste(vec)   %>%
+      writeLines(con = paste0(output_dir, "/citation_keys.txt"))
+    
+    if(verbose == TRUE){
+      cat(paste0("Wrote ", output_dir, "/citation_keys.txt") )
+    }
+    
+  }
+  
+  #    install.packages("pkgsearch")
+  #    library(pkgsearch)
+  #    pkg_info <- pkgsearch::cran_package("dplyr", version = "1.1.4")
+  #    pkg_info$`Date/Publication`
+  
+  
+  pkgs_to_cite_df <- utils::installed.packages()[pkgs_to_cite, "Version"] %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column("Package") %>%
+    dplyr::rename("Version" = "." )
+  
+  if(print_tsv == TRUE ){
     pkgs_to_cite_df %>%
-        readr::write_tsv(paste0(output_dir,"/pkgs_versions_table.tsv"))
-
-        if(verbose == TRUE){
-            cat(paste0("Wrote ", output_dir,"/pkgs_versions_table.tsv \n"))
-        }
-
-
-
+      readr::write_tsv(paste0(output_dir,"/pkgs_versions_table.tsv"))
+    
+    if(verbose == TRUE){
+      cat(paste0("Wrote ", output_dir,"/pkgs_versions_table.tsv \n"))
     }
-
-    if(print_LaTeX_table == TRUE ){
-
-        fn_out = paste0(output_dir, "/used_packages_table.tex")
-        cap <- "Table of R-packages used."
-        lbl <- "r_package_table"
-        align <- c("r","p{2.5cm}","p{2.5cm}")
-
-        pkgs_to_cite_df %>%
-            xtable::xtable(caption = cap, label = lbl,
-                   align = align) %>%
-            xtable::print.xtable(file = fn_out,
-                                 sanitize.colnames.function = function(x){x},
-                                 sanitize.text.function = function(x){x},
-                                 include.rownames = FALSE, math.style.negative = F,tabular.environment = "longtable",
-                                 booktabs = TRUE, floating = F)
-
-        if(verbose == TRUE){
-            cat(paste0("Wrote ", fn_out, "\n" ))
-        }
-
-
+    
+    
+    
+  }
+  
+  if(print_LaTeX_table == TRUE ){
+    
+    fn_out = paste0(output_dir, "/used_packages_table.tex")
+    cap <- "Table of R-packages used."
+    lbl <- "r_package_table"
+    align <- c("r","p{2.5cm}","p{2.5cm}")
+    
+    pkgs_to_cite_df %>%
+      xtable::xtable(caption = cap, label = lbl,
+                     align = align) %>%
+      xtable::print.xtable(file = fn_out,
+                           sanitize.colnames.function = function(x){x},
+                           sanitize.text.function = function(x){x},
+                           include.rownames = FALSE, math.style.negative = F,tabular.environment = "longtable",
+                           booktabs = TRUE, floating = F)
+    
+    if(verbose == TRUE){
+      cat(paste0("Wrote ", fn_out, "\n" ))
     }
-
-
+    
+    
+  }
+  
+  
 }
 
 
 .list.functions.in.file_SH <- function (filename)
 {
-    if (!file.exists(filename)) {
-        stop("couldn't find file ", filename)
-    }
-    if (!reader::get.ext(filename) == "R") {
-        warning("expecting *.R file, will try to proceed")
-    }
-    tmp <- utils::getParseData(parse(filename, keep.source = TRUE))
-    funs <- tmp$text[which(tmp$token == "SYMBOL_FUNCTION_CALL")]
-
-    df <- data.frame("functions" = funs)
-    df[,2] <- NA #make empty col to fill in for-loop
-    colnames(df) <- c("functions", "packages")
-
-    for(i in 1:nrow(df)){
-
-        funs <- utils::find(df[i,1])
-        df[i,2] <- paste0(funs, collapse = ";")
-    }
-
-
-
-
-
-    return(df)
+  if (!file.exists(filename)) {
+    stop("couldn't find file ", filename)
+  }
+  if (!reader::get.ext(filename) == "R") {
+    warning("expecting *.R file, will try to proceed")
+  }
+  tmp <- utils::getParseData(parse(filename, keep.source = TRUE))
+  funs <- tmp$text[which(tmp$token == "SYMBOL_FUNCTION_CALL")]
+  
+  df <- data.frame("functions" = funs)
+  df[,2] <- NA #make empty col to fill in for-loop
+  colnames(df) <- c("functions", "packages")
+  
+  for(i in 1:nrow(df)){
+    
+    funs <- utils::find(df[i,1])
+    df[i,2] <- paste0(funs, collapse = ";")
+  }
+  
+  
+  
+  
+  
+  return(df)
 }
 
 #credit_packages(fns = fns <- list.files(path = "../../../Nextcloud/Hedvigs_academia/2024/emergent_interface/Emergent_interface_Hedvig/", pattern = "*.[R|r]$", full.names = T, recursive = F), output_dir = "../../../Nextcloud/Hedvigs_academia/2024/emergent_interface/Emergent_interface_Hedvig/output/package_versions/")
